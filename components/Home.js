449268a7d73cdd2a74bc0020bc3663e680efc2ec
Yanis <yanisbenazz1@gmail.com>
2024-05-02 12:46:21 +0200

    pagehome

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,26 @@
 import styles from '../styles/Home.module.css';
+import { useState } from "react";
+import React from 'react'
+import Image from 'next/image';
+
 
 function Home() {
+
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className={styles.container}>
+      <div className= {styles.image}> 
+      bonjour
+      </div>
+      <Image className={styles.logo} src="/logo.png" alt="logo" width={50} height={50} />
+      <h1 className={styles.title}>See what's happening</h1>
+      <p className={styles.join}>Join Hackatweet today.</p>
+      <button className={styles.signup}>Sign up</button>
+      <p className={styles.account}>Already have an account</p>
+      <button className={styles.signin}>Sign in</button>
     </div>
-  );
+  )
 }
 
 export default Home;
+
+
